f5f0c878d4850e0a8d90f76ecabb62853a533dcb
Nadya Chistyakova <npodtynchenko@list.ru>
2020-11-21 15:43:38 +0300

    edit test

--- src/test/java/ru/netology/service/CashbackHackServiceTest.java
+++ src/test/java/ru/netology/service/CashbackHackServiceTest.java
@@ -12,7 +12,7 @@ class CashbackHackServiceTest {
         int amount = 2000;
 
         int actual = cashbackHackService.remain(amount);
-        int expected = 1000;
+        int expected = 0;
 
         assertEquals(expected,actual);
     }
